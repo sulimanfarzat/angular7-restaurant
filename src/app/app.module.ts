c666e04baa6b77eb554689ca0f089457519f0e55
Suliman Farzat <s_farzat@outlook.com>
2019-01-27 23:53:16 +0100

    Components Part 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/dishDetail.ts
@@ -0,0 +1,11 @@
+export class DishDetail {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments: any[];
+}
