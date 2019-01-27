ed3da7ffc3452bf20860952866491210127c39df
Suliman Farzat <s_farzat@outlook.com>
2019-01-27 22:27:08 +0100

    Components Part 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 @Component({
   selector: 'app-menu',
@@ -51,7 +52,7 @@ export class MenuComponent implements OnInit {
       description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
     }
    ];
-   
+
   constructor() { }
 
   ngOnInit() {
